35e77abf7301546e3a5b467540844818d79074ee
Ceres Rohana <ceres.rohana@gmail.com>
2017-10-23 15:28:56 -0200

    Add sample application + tests.

---
+++ test/MyAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MyAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(2 == 2);
+        }
+    }
+}
